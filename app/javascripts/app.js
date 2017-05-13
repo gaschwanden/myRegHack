c579e6fff3244556c7a47e722908b53d88668068
Prabhat 'David' Shrestha <tasker.prabhat@gmail.com>
2017-05-14 01:28:16 +1000

    stupid changes

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -75,9 +75,10 @@ window.App = {
     var registry;
     LandRegistry.deployed().then(function(instance) {
       registry = instance;
-      registry.registerLand(owner, physicalAddress);
-      return self.getLandByOwner(owner);
-    }).then(function(landId) {
+      return registry.registerLand(owner, physicalAddress);
+    }).then(function() {
+      landId = self.getLandByOwner(owner);
+      console.log(landId);
       self.setStatus('Land registered with ID: ' + landId);
     }).catch(function(e) {
       console.log(e);
@@ -91,7 +92,7 @@ window.App = {
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.warn("Using web3 detected from external source. Ensure you've configured that source properly. If using MetaMask, see the following link.")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
